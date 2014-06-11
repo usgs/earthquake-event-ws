b3ae1fb644f2833ed16e14a517f43d1ac098a73e
Eric Martinez <emartinez@usgs.gov>
2014-06-11 09:06:24 -0600

    Switched order to DFYI->SM->PAGER. Renamed some layers. Supress fault.kmz when empty-ish. Preferring overlay image rather than kmz. Added DYFI legend.

--- src/lib/classes/detail/kmlraw.inc.php
+++ src/lib/classes/detail/kmlraw.inc.php
@@ -71,7 +71,13 @@
 
 		if (isset($smc['download/fault.kmz'])) {
 			$smfault = $smc['download/fault.kmz'];
-			$include_sm = true;
+
+			// Hack to "detect" if fault.kmz is "empty"
+			if (abs(intval($smfault['length']) - 600) < 50) {
+				$smfault = null;
+			} else {
+				$include_sm = true;
+			}
 		}
 
 		if (isset($smc['download/overlay.kmz'])) {
@@ -194,123 +200,22 @@
 					'<listItemType>radioFolder</listItemType>' .
 				'</ListStyle></Style>';
 
-		// PAGER
-		if ($lpe) {
-			echo "\n" . '<Folder>' .
-					'<name>PAGER - ' . ucwords($summary['alertlevel']) . '</name>' .
-					$kmlfeed->getLookAt($latitude, $longitude, 100000, 70) .
-					'<NetworkLink>' .
-						'<name>Exposure</name>' .
-						'<visibility>0</visibility>' .
-						'<Link>' .
-							'<href>' . $lpe['url'] . '</href>' .
-							'<viewRefreshMode>never</viewRefreshMode>' .
-						'</Link>' .
-					'</NetworkLink>' .
-				'</Folder>';
-		} // end PAGER
-
-
-		// ShakeMap
-		if ($include_sm) {
-			echo "\n" . '<Folder>' .
-					'<name>ShakeMap - ' .
-						$ROMANS[intval($summary['maxmmi'])] .
-					'</name>';
-
-			if ($smcont) {
-				echo '<NetworkLink>' .
-						'<name>Contours</name>' .
-						'<visibility>1</visibility>' .
-						'<Link>' .
-							'<href>' . $smcont['url'] . '</href>' .
-							'<viewRefreshMode>never</viewRefreshMode>' .
-						'</Link>' .
-					'</NetworkLink>';
-			}
-
-			if ($smfault) {
-				echo '<NetworkLink>' .
-						'<name>Faults</name>' .
-						'<visibility>0</visibility>' .
-						'<Link>' .
-							'<href>' . $smfault['url'] . '</href>' .
-							'<viewRefreshMode>never</viewRefreshMode>' .
-						'</Link>' .
-					'</NetworkLink>';
-			}
-
-			if ($smover) {
-				echo '<NetworkLink>' .
-						'<name>Intensity Overlay</name>' .
-						'<visibility>0</visibility>' .
-						'<Link>' .
-							'<href>' . $smover['url'] . '</href>' .
-							'<viewRefreshMode>never</viewRefreshMode>' .
-						'</Link>' .
-					'</NetworkLink>';
-			} else if ($smo && isset($smp['maximum-latitude'])) {
-				echo $kmlfeed->getLookAt($latitude, $longitude, 500000) .
-					'<GroundOverlay>' .
-						'<name>Intensity</name>' .
-						'<color>9EFFFFFF</color>' .
-						'<visibility>0</visibility>' .
-						'<drawOrder>1</drawOrder>' .
-						'<Icon>' .
-							'<refreshMode>onChange</refreshMode>' .
-							'<href>' . $smo['url'] . '</href>' .
-						'</Icon>' .
-						'<LatLonBox>' .
-							'<north>' . $smp['maximum-latitude'] . '</north>' .
-							'<south>' . $smp['minimum-latitude'] . '</south>' .
-							'<east>' . $smp['maximum-longitude'] . '</east>' .
-							'<west>' . $smp['minimum-longitude'] . '</west>' .
-						'</LatLonBox>' .
-					'</GroundOverlay>';
-			}
-
-			if ($sms) {
-				echo '<NetworkLink>' .
-						'<name>Stations</name>' .
-						'<visibility>0</visibility>' .
-						'<Link>' .
-							'<href>' . $sms['url'] . '</href>' .
-							'<refreshMode>onExpire</refreshMode>' .
-						'</Link>' .
-					'</NetworkLink>';
-			}
-
-			// If any of the colored layers are present, thus the legend is relevant,
-			// include the legend overlay. This may be overkill logic since one of
-			// these ought always exist...
-			if ($smover || $smcont || ($smo && isset($smp['maximum-latitude']))) {
-				echo '<ScreenOverlay>' .
-						'<name>Intensity Legend</name>' .
-						'<visibility>0</visibility>' .
-						'<Icon><href>' .
-							$HOST_URL_PREFIX . $FEED_PATH . '/images/kml_shakemap_legend.png' .
-						'</href></Icon>' .
-						'<overlayXY x="0" y="90" xunits="pixels" yunits="pixels"/>' .
-						'<screenXY x="5" y="1" xunits="pixels" yunits="fraction"/>' .
-						'<size x="0" y="0" xunits="pixels" yunits="pixels"/>' .
-					'</ScreenOverlay>';
-			}
-
-			echo '</Folder>';
-
-		} // end ShakeMap
-
-
 		if ($dyfizip || $dyfigeo) {
 			echo '<Folder>' .
 					'<name>DYFI - ' .
 						$ROMANS[intval($summary['maxcdi'])] .
 					'</name>' .
 					$kmlfeed->getLookAt($latitude, $longitude, 500000) .
-				'<visibility>0</visibility>' .
-				'<Style><ListStyle>' .
-					'<listItemType>radioFolder</listItemType>' .
-				'</ListStyle></Style>';
+				'<visibility>0</visibility>';
+
+			if ($dyfizip && $dyfigeo) {
+				echo '<Folder>' .
+					'<name>Reported Intensity</name>' .
+					'<visibility>0</visibility>' .
+					'<Style><ListStyle>' .
+						'<listItemType>radioFolder</listItemType>' .
+					'</ListStyle></Style>';
+			}
 
 			if ($dyfizip) {
 				echo '<NetworkLink>' .
@@ -332,11 +237,132 @@
 							'<refreshMode>onExpire</refreshMode>' .
 						'</Link>' .
 					'</NetworkLink>';
-			};
+			}
+
+			if ($dyfizip && $dyfigeo) {
+				echo '</Folder>';
+			}
+
+			echo '<ScreenOverlay>' .
+				'<name>DYFI Intensity Legend</name>' .
+				'<visibility>0</visibility>' .
+				'<Icon><href>' .
+					$HOST_URL_PREFIX . $FEED_PATH . '/images/kml_dyfi_legend.png' .
+				'</href></Icon>' .
+				'<overlayXY x="0" y="90" xunits="pixels" yunits="pixels"/>' .
+				'<screenXY x="5" y="1" xunits="pixels" yunits="fraction"/>' .
+				'<size x="0" y="0" xunits="pixels" yunits="pixels"/>' .
+			'</ScreenOverlay>' .
+			'</Folder>';
+
+		} // end DYFI
+
+
+		// ShakeMap
+		if ($include_sm) {
+			echo "\n" . '<Folder>' .
+					'<name>ShakeMap - ' .
+						$ROMANS[intval($summary['maxmmi'])] .
+					'</name>';
+
+			if ($smcont) {
+				echo '<NetworkLink>' .
+						'<name>Contours</name>' .
+						'<visibility>1</visibility>' .
+						'<Style><ListStyle>' .
+							'<listItemType>radioFolder</listItemType>' .
+						'</ListStyle></Style>' .
+						'<Link>' .
+							'<href>' . $smcont['url'] . '</href>' .
+							'<viewRefreshMode>never</viewRefreshMode>' .
+						'</Link>' .
+					'</NetworkLink>';
+			}
+
+			if ($smfault) {
+				echo '<NetworkLink>' .
+						'<name>Faults</name>' .
+						'<visibility>0</visibility>' .
+						'<Link>' .
+							'<href>' . $smfault['url'] . '</href>' .
+							'<viewRefreshMode>never</viewRefreshMode>' .
+						'</Link>' .
+					'</NetworkLink>';
+			}
+
+			if ($smo && isset($smp['maximum-latitude'])) {
+				echo $kmlfeed->getLookAt($latitude, $longitude, 500000) .
+					'<GroundOverlay>' .
+						'<name>Intensity Overlay</name>' .
+						'<color>FFFFFFFF</color>' .
+						'<visibility>0</visibility>' .
+						'<drawOrder>0</drawOrder>' .
+						'<Icon>' .
+							'<refreshMode>onChange</refreshMode>' .
+							'<href>' . $smo['url'] . '</href>' .
+						'</Icon>' .
+						'<LatLonBox>' .
+							'<north>' . $smp['maximum-latitude'] . '</north>' .
+							'<south>' . $smp['minimum-latitude'] . '</south>' .
+							'<east>' . $smp['maximum-longitude'] . '</east>' .
+							'<west>' . $smp['minimum-longitude'] . '</west>' .
+						'</LatLonBox>' .
+					'</GroundOverlay>';
+			} else if ($smover) {
+				echo '<NetworkLink>' .
+						'<name>Intensity Overlay</name>' .
+						'<visibility>0</visibility>' .
+						'<Link>' .
+							'<href>' . $smover['url'] . '</href>' .
+							'<viewRefreshMode>never</viewRefreshMode>' .
+						'</Link>' .
+					'</NetworkLink>';
+			}
+
+			if ($sms) {
+				echo '<NetworkLink>' .
+						'<name>Stations</name>' .
+						'<visibility>0</visibility>' .
+						'<Link>' .
+							'<href>' . $sms['url'] . '</href>' .
+							'<refreshMode>onExpire</refreshMode>' .
+						'</Link>' .
+					'</NetworkLink>';
+			}
+
+			// If any of the colored layers are present, thus the legend is relevant,
+			// include the legend overlay. This may be overkill logic since one of
+			// these ought always exist...
+			if ($smover || $smcont || ($smo && isset($smp['maximum-latitude']))) {
+				echo '<ScreenOverlay>' .
+						'<name>ShakeMap Intensity Legend</name>' .
+						'<visibility>0</visibility>' .
+						'<Icon><href>' .
+							$HOST_URL_PREFIX . $FEED_PATH . '/images/kml_shakemap_legend.png' .
+						'</href></Icon>' .
+						'<overlayXY x="0" y="90" xunits="pixels" yunits="pixels"/>' .
+						'<screenXY x="5" y="1" xunits="pixels" yunits="fraction"/>' .
+						'<size x="0" y="0" xunits="pixels" yunits="pixels"/>' .
+					'</ScreenOverlay>';
+			}
 
 			echo '</Folder>';
 
-		} // end DYFI
+		} // end ShakeMap
+
+
+		// PAGER
+		if ($lpe) {
+			echo "\n" . '<NetworkLink>' .
+						'<name>PAGER - ' . ucwords($summary['alertlevel']) . '</name>' .
+						'<visibility>0</visibility>' .
+						'<Link>' .
+							'<href>' . $lpe['url'] . '</href>' .
+							'<viewRefreshMode>never</viewRefreshMode>' .
+						'</Link>' .
+					'</NetworkLink>';
+		} // end PAGER
+
 
 		echo '</Folder>';
 
