db73afecdb9df2d24f138070db41d6c988505f16
Jeremy Fee <jmfee@usgs.gov>
2015-03-12 14:39:00 -0600

    Redirect from search without a slash to search with a slash

--- src/lib/pre-install.php
+++ src/lib/pre-install.php
@@ -44,6 +44,7 @@ RewriteEngine on
 
 # Only ever offered for v1.0, so just need to redirect that version
 RewriteRule ^/earthquakes/eqarchives/epic/ ' . $SEARCH_PATH . '/ [R=301]
+RewriteRule ^' . $SEARCH_PATH . '$ ' . $SEARCH_PATH . '/ [R=301]
 RewriteRule ^' . $SEARCH_PATH . '/$ ' . $FEED_PATH . '/search.php [L,PT]
 RewriteRule ^' . $SEARCH_PATH . '/(js|css|lib)/(.*) ' . $FEED_PATH .
     '/$1/$2 [L,PT]
