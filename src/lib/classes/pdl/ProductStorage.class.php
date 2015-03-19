b2d4c7ea7c74b40517c803fd69d50d336df1e348
Eric Martinez <emartinez@usgs.gov>
2015-03-19 12:27:56 -0600

    Removed debug print statement.

--- src/lib/classes/pdl/ProductStorage.class.php
+++ src/lib/classes/pdl/ProductStorage.class.php
@@ -42,8 +42,6 @@ class ProductStorage {
     $xml = $directory . '/' . self::$PRODUCT_XML_FILENAME;
     $url = $this->getProductURL($productid) . '/' . self::$PRODUCT_XML_FILENAME;
 
-    echo $url . "\n";
-
     //parse xml
     try {
       if (is_file($xml)) { // Prefer local files
