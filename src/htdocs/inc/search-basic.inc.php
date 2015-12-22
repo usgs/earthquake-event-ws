d13a568de53ad6d5bf3279ff6b1c16be8c5fc708
Eddie McWhirter <emcwhirter@usgs.gov>
2015-12-22 13:11:53 -0700

    Add a search button above the advanced toggle.

--- src/htdocs/inc/search-basic.inc.php
+++ src/htdocs/inc/search-basic.inc.php
@@ -1,4 +1,11 @@
-<section class="one-of-two column" aria-labelledby="location">
-  <h3 id="location" class="label">Choose a location</h3>
-  <button type="button" class="draw orange">Draw Rectangle on Map</button>
-</section>
+<div class="one-of-two column">
+  <section aria-labelledby="location">
+    <h3 id="location" class="label">Choose a location</h3>
+    <button type="button" class="draw orange">Draw Rectangle on Map</button>
+  </section>
+  <section class="footer" aria-label="Search form button">
+    <button type="submit" id="fdsn-submit">Search</button>
+    <span class="output-descriptor"></span>
+    <div class="search-error"></div>
+  </section>
+</div>
