67410cc934e0d535a5d92f835fdbc0c9de32d915
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2016-01-13 14:35:54 -0700

    removed if statmens that are taken care of in there views now. also removed things that are moved to formatter classes

--- src/htdocs/js/fdsn/FDSNModel.js
+++ src/htdocs/js/fdsn/FDSNModel.js
@@ -38,9 +38,8 @@ var NUMERIC_TYPES = {
 
 // Enumeration of all FDSN fields
 var DEFAULT_DATA = {
-  starttime: '',
-  enddtime: '',
-  basictime: null,
+  starttime: null,
+  enddtime: null,
 
   maxlatitude: null,
   minlatitude: null,
@@ -51,8 +50,7 @@ var DEFAULT_DATA = {
   longitude: null,
   maxradiuskm: null,
 
-  basicmagnitude: null,
-  minmagnitude: '',
+  minmagnitude: null,
   maxmagnitdue: null,
 
   mindepth: null,
@@ -94,48 +92,6 @@ var DEFAULT_DATA = {
   //includearrivals: null
 };
 
-var _formatDateTime = function (stamp) {
-  var parts = stamp.split(/( |T)/),
-      ymd = parts[0] || '',
-      hms = parts[1] || '',
-      formattedStamp = null,
-      year = null, month = null, day = null,
-      hours = null, minutes = null, seconds = null;
-
-  try {
-
-    if (parts.length > 2) {
-      throw 'Invalid date format';
-    }
-
-    ymd = ymd.split('-');
-    hms = hms.split(':');
-
-    // Use Date.UTC to perform date math on input values
-    formattedStamp = new Date(Date.UTC(ymd[0] || 0, (ymd[1] - 1) || 0,
-        ymd[2] || 1, hms[0] || 0, hms[1] || 0, hms[2] || 0));
-
-    year = formattedStamp.getUTCFullYear();
-    month = formattedStamp.getUTCMonth() + 1;
-    day = formattedStamp.getUTCDate();
-    hours = formattedStamp.getUTCHours();
-    minutes = formattedStamp.getUTCMinutes();
-    seconds = formattedStamp.getUTCSeconds();
-
-    if (month < 10) { month = '0' + month; }
-    if (day < 10) { day = '0' + day; }
-    if (hours < 10) { hours = '0' + hours; }
-    if (minutes < 10) { minutes = '0' + minutes; }
-    if (seconds < 10) { seconds = '0' + seconds; }
-
-    return year + '-' + month + '-' + day + ' ' + hours + ':' +
-        minutes + ':' + seconds;
-  } catch (e) {
-    // Couldn't parse, use original, let server try
-    return stamp;
-  }
-};
-
 var FDSNModel = function (data) {
   var _parentSet,
       _this;
@@ -148,31 +104,6 @@ var FDSNModel = function (data) {
 
   _this.set = function (params, options) {
     var key = null;
-
-    // Checks for basic time 7 Days, 24 hours, or Custom time.
-    if (params.hasOwnProperty('basictime') && params.basictime !== '') {
-      params.starttime = _formatDateTime(params.basictime);
-    } else {
-      // Format date/time stamps (if possible)
-      if (params.hasOwnProperty('starttime') && params.starttime !== '') {
-        params.starttime = _formatDateTime(params.starttime);
-      }
-    }
-    if (params.hasOwnProperty('endtime') && params.endtime !== '') {
-      params.endtime = _formatDateTime(params.endtime);
-    }
-
-    if (params.hasOwnProperty('basicmagnitude') && params.basictime !== '') {
-      if (params.basicmagnitude === 'greaterfour') {
-        params.minmagnitude = '4';
-        params.maxmagnitude = '';
-      }
-      if (params.basicmagnitude === 'lessfour') {
-        params.minmagnitude = '';
-        params.maxmagnitude = '4';
-      }
-    }
-
 
     // Convert numeric types to numbers (for comparison later)
     for (key in params) {
