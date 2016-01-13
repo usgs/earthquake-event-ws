c367c5bce8fd9b1c12375e33b8eb541ce8466b74
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2016-01-13 14:33:38 -0700

    updated basic search uses views now and has input fields built in

--- src/htdocs/inc/search-basic.inc.php
+++ src/htdocs/inc/search-basic.inc.php
@@ -1,91 +1,61 @@
-<?php
-  date_default_timezone_set('UTC');
-  $now = time();
-  $dayago = $now - 86400; // 24 hours ago
-  $dayago = date('Y-m-d H:i:s', $dayago);
-  $starttime = $now - 604800; // 1 week ago
-  $basictime = $starttime;
-  $basictime = date('Y-m-d H:i:s', $basictime);
-  $starttime -= ($starttime % 86400); // Round to start of day
-  $starttime = date('Y-m-d H:i:s', $starttime);
-
-  $endtime = $now + 86400; // Tomorrow
-  $endtime -= ($endtime % 86400); // Round to start of day (i.e. end of today)
-  $endtime = date('Y-m-d H:i:s', $endtime - 1);
-?>
-
-
-<div class="column">
-  <section aria-labelledby="region">
-    <h3 id="region" class="label">Geographic Region</h3>
-    <div class="region-description"></div>
-
-    <div class="fieldset" role="group" aria-labelledby="region-rectangle">
-      <h4 id="region-rectangle" class="label">Rectangle</h4>
-
-      <button type="button" class="draw orange">Draw Rectangle on Map</button>
-
-    </div>
-  </section>
-</div>
-
-<div class="one-of-two column">
-  <section aria-labelledby="basictime">
-    <h3 id="basictime" class="label">Select Time</h3>
-
-    <ul class="no-style basictime-list">
-      <li>
-        <input id="basictime-weekago" type="radio" name="basictime"
-            value="<?php print $basictime ?>" checked="checked"
-            aria-labelledby="weekago"/>
-        <label for="basictime-weekago" class="label-checkbox">
-          Past 7 Days
-        </label>
-      </li>
-      <li>
-        <input id="basictime-dayago" type="radio" name="basictime"
-            value="<?php print $dayago ?>" aria-labelledby="dayago"/>
-        <label for="basictime-dayago" class="label-checkbox">
-          Past 24 Hours
-        </label>
-      </li>
-      <li>
-        <input id="basictime-custom" type="radio" name="basictime"
-            value="" aria-labelledby="custom"/>
-        <label for="basictime-custom" class="label-checkbox">
-          Custom (set in advanced options)
-        </label>
-      </li>
-    </ul>
-  </section>
-</div>
-
-<div class="one-of-two column">
-  <section aria-labelledby="basicmagnitude">
+<div class="one-of-three column">
+  <section class="magnitude-view">
     <h3 id="magnitude" class="label">Magnitude</h3>
-
-    <ul class="no-style basicmagnitude-list">
+    <ul class="no-style basicmagnitude-list"></ul>
+    <ul class="vertical no-style">
       <li>
-        <input id="greaterfour" type="radio" name="basicmagnitude"
-            value="greaterfour" checked="checked" aria-labelledby="greaterfour"/>
-        <label for="greaterfour" class="label-checkbox">
-          > 4 Stronger
+        <label for="minmagnitude" class="label" id="magnitude-minmagnitude">
+          Minimum
         </label>
+        <input type="number" step="any" name="minmagnitude" id="minmagnitude"
+            min="-1" max="10" step="0.1" value="2.5" disabled="false"
+            aria-labelledby="magnitude magnitude-minmagnitude"/>
       </li>
       <li>
-        <input id="lessfour" type="radio" name="basicmagnitude"
-            value="lessfour" aria-labelledby="lessfour"/>
-        <label for="lessfour" class="label-checkbox">
-          < 4 Weaker
+        <label for="maxmagnitude" class="label" id="magnitude-maxmagnitude">
+          Maximum
         </label>
-      </li>
-      <li>
-        <input id="custommag" type="radio" name="basicmagnitude"
-            value="" aria-labelledby="custommag"/>
-        <label for="custommag" class="label-checkbox">
-          Custom (set in advanced options)
-        </label>
+        <input type="number" step="any" name="maxmagnitude" id="maxmagnitude"
+            min="-1" max="10" step="0.1" value="" disabled="false"
+            aria-labelledby="magnitude magnitude-maxmagnitude"/>
       </li>
     </ul>
+  </section>
+</div>
+
+<div class="one-of-three column">
+  <section class="date-time-view">
+    <h3 id="datetime" class="label">Date &amp; Time</h3>
+    <ul class="no-style basictime-list"></ul>
+    <ul class="vertical no-style">
+      <li>
+        <label for="starttime" class="label" id="datetime-starttime">
+          Start (UTC)
+        </label>
+        <input type="text" name="starttime" id="starttime"
+            placeholder="yyyy-mm-dd hh:mm:ss"
+
+            disabled="false"
+            aria-labelledby="datetime datetime-starttime"/>
+      </li>
+      <li>
+        <label for="endtime" class="label" id="datetime-endtime">
+          End (UTC)
+        </label>
+        <input type="text" name="endtime" id="endtime"
+            placeholder="yyyy-mm-dd hh:mm:ss"
+
+            disabled="false"
+            aria-labelledby="datetime datetime-endtime"/>
+      </li>
+    </ul>
+  </section>
+</div>
+
+<div class="one-of-three column">
+  <section class="location-view">
+    <h3 id="region" class="label">Geographic Region</h3>
+    <ul class="no-style basiclocation-list"></ul>
+    <div class="region-description"></div>
   </section>
 </div>
