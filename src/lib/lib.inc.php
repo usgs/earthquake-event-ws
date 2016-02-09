9bdb1fc9cc6d38664465fd5ebaf025beafb2685f
Jeremy Fee <jmfee@usgs.gov>
2016-02-09 15:23:10 -0700

    Add and use safe_json_encode method, for malformed UTF8 characters

--- src/lib/lib.inc.php
+++ src/lib/lib.inc.php
@@ -52,3 +52,38 @@
       }
     }
   }
+
+  if (!function_exists("safe_json_encode")) {
+    // from http://stackoverflow.com/questions/10199017/how-to-solve-json-error-utf8-error-in-php-json-decode
+    function safe_json_encode($value){
+      $encoded = json_encode($value);
+      switch (json_last_error()) {
+        case JSON_ERROR_NONE:
+          return $encoded;
+        case JSON_ERROR_DEPTH:
+          return 'Maximum stack depth exceeded'; // or trigger_error() or throw new Exception()
+        case JSON_ERROR_STATE_MISMATCH:
+          return 'Underflow or the modes mismatch'; // or trigger_error() or throw new Exception()
+        case JSON_ERROR_CTRL_CHAR:
+          return 'Unexpected control character found';
+        case JSON_ERROR_SYNTAX:
+          return 'Syntax error, malformed JSON'; // or trigger_error() or throw new Exception()
+        case JSON_ERROR_UTF8:
+          $clean = utf8ize($value);
+          return safe_json_encode($clean);
+        default:
+          return 'Unknown error'; // or trigger_error() or throw new Exception()
+      }
+    }
+
+    function utf8ize($mixed) {
+      if (is_array($mixed)) {
+          foreach ($mixed as $key => $value) {
+              $mixed[$key] = utf8ize($value);
+          }
+      } else if (is_string ($mixed)) {
+          return utf8_encode($mixed);
+      }
+      return $mixed;
+    }
+  }
