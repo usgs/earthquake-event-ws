d7974a8fbf91bedfce672ee924e67ea82fb1707d
Eddie Hunter <ehunter@usgs.gov>
2017-06-12 14:45:11 -0600

    escape apostrophe

--- src/htdocs/search.php
+++ src/htdocs/search.php
@@ -55,7 +55,7 @@ if (!isset($TEMPLATE)) {
           </li>
           <li>
             <a href="https://github.com/usgs/devcorner">
-              Developer's Corner - bulk access to catalog, tools for obtaining
+              Developer\'s Corner - bulk access to catalog, tools for obtaining
               specific products
             </a>
           </li>
