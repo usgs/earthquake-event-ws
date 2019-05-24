b33bea8bb63a66084458689cf2185c5afdd621c0
Bass <cbass@igskcicgwsgm015.gs.doi.net>
2019-05-24 13:17:21 -0600

    Made more stylistic changes

--- src/lib/classes/pdl/ProductQuery.class.php
+++ src/lib/classes/pdl/ProductQuery.class.php
@@ -1,12 +1,22 @@
 <?php
 
 //Stores all the information that can be queried for a product
-class ProductQuery{
+class ProductQuery {
   
   public $source;
   public $type;
   public $code;
   public $updateTime;
+
+  public $startTime;
+  public $endTime;
+  public $maxLatitude;
+  public $maxLongitude;
+  public $minLatitude;
+  public $minLongitude;
+  public $latitude;
+  public $longitude;
+  public $time;
 
   //path search term (to be implemented)
   //public $path = null; 
@@ -20,7 +30,8 @@ class ProductQuery{
   }
   */
 
-  public function toString(){
+  //Depricated as of 281b
+  public function toString() {
     $ret = "Parameters: (" . $this->source . ", " . $this->type . ", " . $this->code . ", " . $this->updateTime . ")\n\r";
     return $ret;
   }
