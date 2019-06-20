ad1490c299b5eec50540b7359ffd9a95ad1f8b8b
Bass <cbass@igskcicgwsgm015.cr.usgs.gov>
2019-06-20 14:21:42 -0600

    Parented error handling

--- src/lib/classes/pdl/WebService.class.php
+++ src/lib/classes/pdl/WebService.class.php
@@ -12,8 +12,6 @@ class WebService {
   // maximum number of search results
   public $serviceLimit;
 
-  public $redirect;
-
   //Error codes
   const NO_DATA = 204;
   const BAD_REQUEST = 400;
@@ -45,6 +43,73 @@ class WebService {
 
     global $FDSN_VERSION;
     $this->version = $FDSN_VERSION;
+  }
+
+  //Error handling
+  public function error($code, $message, $isDetail = false) {
+    global $APP_DIR;
+
+    // only cache errors for 60 seconds
+    $CACHE_MAXAGE = 60;
+    include $APP_DIR . '/lib/cache.inc.php';
+
+    if (isset($_GET['jsonerror']) && $_GET['jsonerror'] == 'true' &&
+        isset($_GET['format']) && $_GET['format'] == 'geojson') {
+      // For geojson requests, user wants 'jsonerror' output
+      $this->jsonError($code, $message, $isDetail);
+    } else {
+      $this->httpError($code, $message);
+    }
+  }
+
+  //Prints error if user expecting JSON
+  public function jsonError ($code, $message, $isDetail = false) {
+    global $HOST_URL_PREFIX;
+    $callback = false;
+    if (isset($_GET['callback'])) {
+      $callback = $_GET['callback'];
+      // restrict allowed callback names
+      if (!preg_match('/^[A-Za-z0-9\._]+$/', $callback)) {
+        header('HTTP/1.0 400 Bad Request');
+        echo 'Bad callback value, valid characters include [A-Za-z0-9\._]';
+        exit();
+      }
+      header('Content-type: text/javascript');
+    } else {
+      header('Content-type: application/json');
+    }
+
+    // Does this need to look fully like GeoJSON format?
+    $response = array(
+      'type' => $isDetail ? 'Feature' : 'FeatureCollection',
+      'metadata' => array(
+        'status' => $code,
+        'generated' => time() . '000',
+        'url' => $HOST_URL_PREFIX . $_SERVER['REQUEST_URI'],
+        'title' => 'Search Error',
+        'api' => $this->version,
+        'count' => 0,
+        'error' => $message
+      )
+    );
+
+    if ($isDetail) {
+      $response['properties'] = null;
+    } else {
+      $response['features'] = array();
+    }
+
+    if ($callback) {
+      echo $callback . '(';
+    }
+    echo preg_replace('/"(generated)":"([\d]+)"/', '"$1":$2',
+        str_replace('\/', '/', safe_json_encode($response)));
+
+    if ($callback) {
+      echo ');';
+    }
+
+    exit();
   }
 
   //Error for all other requests
