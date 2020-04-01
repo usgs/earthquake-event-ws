19950a2cf4d0e284217be141b297312ddd0b34e1
Jeremy Fee <jmfee@usgs.gov>
2020-04-01 16:36:18 -0600

    table to track preferred origin products, and stored procedures to populate it

---
+++ src/lib/sql/fdsnws/productSummaryEventStatus.sql
@@ -0,0 +1,100 @@
+CREATE TABLE IF NOT EXISTS productSummaryEventStatus (
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  eventId BIGINT NOT NULL,
+  eventUpdated BIGINT NOT NULL,
+  productSummaryId BIGINT NOT NULL,
+  eventPreferred INT DEFAULT 0,
+
+  KEY preferredEventProductIndex (eventId, eventPreferred),
+  FOREIGN KEY (eventId) REFERENCES event(id) ON DELETE CASCADE,
+  FOREIGN KEY (productSummaryId) REFERENCES productSummary(id) ON DELETE CASCADE
+) ENGINE=InnoDB;
+
+delimiter //
+DROP PROCEDURE IF EXISTS updateProductSummaryEventStatus //
+CREATE PROCEDURE updateProductSummaryEventStatus(IN in_eventid INT)
+  MODIFIES SQL DATA
+BEGIN
+  DECLARE preferredId INT;
+
+  -- run in a transaction
+  START TRANSACTION;
+
+  -- remove existing summary status
+  DELETE FROM productSummaryEventStatus WHERE eventId = in_eventid;
+
+  -- add catalog preferred origin products
+  INSERT INTO productSummaryEventStatus
+    (eventId, eventUpdated, productSummaryId, eventPreferred)
+    SELECT
+      e.id as eventId,
+      e.updated as eventUpdated,
+      ps.id as productSummaryId,
+      0 as eventPreferred
+    FROM event e
+    JOIN preferredProduct ps ON (ps.eventid = e.id)
+    WHERE e.id = in_eventid
+    AND ps.type in ('origin', 'origin-scenario');
+
+  -- find preferred origin
+  SELECT id into preferredId FROM productSummary
+  WHERE eventid = in_eventid
+  AND id IN (
+    SELECT productSummaryId
+    FROM productSummaryEventStatus
+    WHERE eventId = in_eventid
+  )
+  ORDER BY preferred DESC, updateTime DESC
+  LIMIT 1;
+
+  -- set preferred origin
+  UPDATE productSummaryEventStatus
+  SET eventPreferred = 1
+  WHERE eventId = in_eventid
+  AND productSummaryId = preferredId;
+
+  -- done
+  COMMIT;
+
+END;
+//
+delimiter ;
+
+
+delimiter //
+DROP PROCEDURE IF EXISTS summarizeProductSummaryEventStatus //
+CREATE PROCEDURE summarizeProductSummaryEventStatus()
+  MODIFIES SQL DATA
+BEGIN
+  DECLARE eventid INT;
+
+  DECLARE done INT DEFAULT 0;
+  DECLARE cur_events CURSOR FOR
+    SELECT DISTINCT e.id
+    FROM event e
+    JOIN preferredProduct ps ON (ps.eventid = e.id)
+    LEFT JOIN productSummaryEventStatus pses ON (pses.eventId = e.id)
+    WHERE ps.type in ('origin', 'origin-scenario')
+    AND (
+      pses.eventUpdated IS NULL
+      OR pses.eventUpdated < e.updated
+    )
+    LIMIT 100000;
+
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+  -- loop over all events, updating eventSummary table
+  OPEN cur_events;
+  cur_events_loop: LOOP
+    FETCH cur_events INTO eventid;
+    IF done = 1 THEN
+      CLOSE cur_events;
+      LEAVE cur_events_loop;
+    END IF;
+
+    CALL updateProductSummaryEventStatus(eventid);
+  END LOOP cur_events_loop;
+
+  COMMIT;
+END;
+//
+delimiter ;
