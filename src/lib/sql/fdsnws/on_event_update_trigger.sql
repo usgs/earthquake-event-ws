8eac9efc278fcf1eb6ca6ee6645e297b988a8beb
Jeremy Fee <jmfee@usgs.gov>
2020-04-01 16:36:43 -0600

    Update on_event_update trigger to call new stored procedure

--- src/lib/sql/fdsnws/on_event_update_trigger.sql
+++ src/lib/sql/fdsnws/on_event_update_trigger.sql
@@ -1,8 +1,12 @@
+-- potentially repetitive, but does not hurt
+DROP TRIGGER IF EXISTS on_event_update_trigger;
+
 delimiter //
 CREATE TRIGGER on_event_update_trigger AFTER UPDATE ON event FOR EACH ROW
 BEGIN
   CALL updateEventSummary(NEW.id);
   CALL summarizeEventProducts(NEW.id);
+  CALL updateProductSummaryEventStatus(NEW.id);
 END;
 //
 delimiter ;
